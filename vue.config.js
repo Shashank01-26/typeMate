9ecfaec0193e996676d93071f5faa8ee18c58c42
Shashank01-26 <srivastavashashank033@gmail.com>
2026-04-13 18:20:59 +0530

    Fixed vue config for favicon injection

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,24 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
-    name: 'Smart Text Suggestion',
-    themeColor: '#962CFF'
+    name: 'TypeMate',
+    themeColor: '#0B1120',
+    msTileColor: '#0B1120',
+    iconPaths: {
+      faviconSVG: 'img/icons/favicon-dark.svg',
+      favicon32: null,
+      favicon16: null,
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    }
   },
   chainWebpack: config => {
-	config
-		.plugin('html')
-		.tap(args => {
-			args[0].title = 'Smart Text Suggestion'
-			return args
-		})
-	}
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'TypeMate'
+        return args
+      })
+  }
 })
